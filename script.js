2f94939666ef530c6348974b91c9815ffa59fdfb
Enitsoa-web <enitsoa.haingonjaramanana@gmail.com>
2026-06-08 17:15:03 +0300

    Jour 3 conditions et logique

--- script.js
+++ script.js
@@ -50,3 +50,59 @@ else {
 document.body.style.fontFamily = "Arial, sans-serif";*/
 
 document.body.style.cssText = "color: blue; font-family: Arial, sans-serif;";
+
+/*Conditions et interactions utilisateur Jour 3*/
+/*Excercice 1*/
+
+/*let age = 20;*/
+let age = 16;
+if (age >= 18) {
+    console.log("Vous êtes majeur.");
+} else {
+    console.log("Vous êtes mineur.");
+}
+
+/*Exercice 2 : Comprendre ===*/
+let nombre = 5;
+if (nombre === 5) {
+    console.log("Le nombre est égal à 5.");
+}
+let estConnecte = true;
+if (estConnecte) {
+    console.log("L'utilisateur est connecté.");
+}   else {
+    console.log("Veuillez vous connecter pour accéder à cette fonctionnalité.");
+}
+
+/*Mini projet : Mini projet Elementor verification stock*/
+let stock = 5;
+let etat = document.querySelector("#etatstock");
+if (stock > 0) {
+    etat.textContent = "En stock";
+    //document.getElementById("etatstock").textContent = "En stock";
+} else {
+    //document.getElementById("etatstock").textContent = "Rupture de stock";
+    etat.textContent = "Rupture de stock";
+}
+
+/*defi jour 3*/
+let nom = "Haingo";
+let estconnecte = true;
+let messsage_connexion = document.querySelector("#message_connexion");
+if (estconnecte) {
+    console.log(`Bienvenue, ${nom} !`);
+    messsage_connexion.textContent = `Bienvenue, ${nom} !`;
+} else {
+    console.log("Veuillez vous connecter pour accéder à votre compte.");
+    messsage_connexion.textContent = "Veuillez vous connecter pour accéder à votre compte.";
+}
+
+/*Mini défi bonus*/
+let montant = 250000;
+let promo = document.querySelector("#promo");
+
+if(montant >= 200000) {
+    promo.textContent = `Pour ${montant} Ar, vous bénéficiez de la livraison gratuite !`;
+} else {
+    promo.textContent = `Pour ${montant} Ar, la livraison est de 5000 Ar.`;
+}
